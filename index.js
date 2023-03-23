8583e1ac6e7359cbbc2d536631d1c0a02c3bd60c
Abdulrahman <albakkar997@gmail.com>
2023-03-23 03:43:31 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function countDown(){
+    for(let i = 10; i >= 0; i--){
+        console.log(i)
+    }
+}
+
+function writeCards(names, event){
+    let messages = []
+    for(let i = 0; i < names.length; i++){
+        messages.push(`Thank you, ` + names[i] + `, for the wonderful ` + event + ` gift!`)
+    }
+    return messages
+}
